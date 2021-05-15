b5943feaa33a52e72d9b265e344363a840d9612e
Mark Wardle <mark@wardle.org>
2021-05-15 09:57:59 +0100

    Start splitting server into modules.
    In preparation for other domains.

--- pc4-server/src/com/eldrix/pc4/server/system.clj
+++ pc4-server/src/com/eldrix/pc4/server/system.clj
@@ -1,7 +1,14 @@
 (ns com.eldrix.pc4.server.system
-  "Composes building blocks into a system using aero, integrant and pathom."
+  "Composes building blocks into a system using aero, integrant and pathom.
+
+  We currently manage the system configuration using integrant - stitching
+  together discrete services. Each of those are then made available for use,
+  but we also build a set of pathom resolvers and operations on top of that
+  functionality. Currently, we manage those resolvers using a simple clojure
+  atom and then build a pathom environment from that. It is conceivable that
+  an integrant component could support defining in-built resolvers itself
+  by the implementation of a protocol, for example."
   (:require [aero.core :as aero]
-            [buddy.sign.jwt :as jwt]
             [clojure.java.io :as io]
             [clojure.spec.alpha :as s]
             [clojure.tools.logging.readable :as log]
@@ -13,6 +20,7 @@
             [com.eldrix.hermes.core :as hermes]
             [com.eldrix.hermes.graph]
             [com.eldrix.pc4.server.api :as api]
+            [com.eldrix.pc4.server.users :as users]
             [com.wsscode.pathom3.connect.indexes :as pci]
             [com.wsscode.pathom3.connect.operation :as pco]
             [com.wsscode.pathom3.connect.built-in.resolvers :as pbir]
@@ -21,8 +29,7 @@
             [com.wsscode.pathom3.interface.eql :as p.eql]
             [integrant.core :as ig]
             [io.pedestal.http :as http]
-            [io.pedestal.interceptor :as intc])
-  (:import (java.time Instant)))
+            [io.pedestal.interceptor :as intc]))
 
 (def resolvers (atom []))
 
@@ -103,106 +110,9 @@
 (defmethod ig/halt-key! :http/server [_ service-map]
   (http/stop service-map))
 
-;;;;;;;;;;;;
-;; Graph resolvers
-
-
-(defn make-user-token
-  [creds {:keys [jwt-expiry-seconds jwt-secret-key]}]
-  (jwt/sign (assoc creds :exp (.plusSeconds (Instant/now) (or jwt-expiry-seconds (* 60 5)))) jwt-secret-key))
-
-(pco/defmutation refresh-token-operation
-  "Refresh the user token.
-  Parameters:
-    |- :token              : the existing token
-
-  Returns a new token."
-  [{:com.eldrix.pc4/keys [login] :as env} {:keys [token]}]
-  {::pco/op-name 'pc4.users/refresh-token
-   ::pco/params  [:token]
-   ::pco/output  [:io.jwt.token]}
-  (when-let [current-user (try (jwt/unsign token (:jwt-secret-key login))
-                               (catch Exception e (log/debug "Attempt to refresh invalid token")))]
-    (log/info "Issuing refreshed token for user " current-user)
-    (make-user-token current-user login)))
-
-(pco/defmutation login-operation
-  "Perform a login.
-  Parameters:
-    |- :system             : the namespace system to use
-    |- :value              : the username.
-    |- :password           : the password.
-  Returns a user.
-
-  It would seem sensible to make a LoginProtocol for each provider, but
-  we still need to map to the keys specified here so it is simpler to use
-  `cond` and choose the correct path based on the namespace and the providers
-  available at runtime."
-  [{:com.eldrix.pc4/keys [login] :as env} {:keys [system value password]}]
-  {::pco/op-name 'pc4.users/login
-   ::pco/params  [:system :value :password]
-   ::pco/output  [:urn.oid.1.2.840.113556.1.4/sAMAccountName ;; sAMAccountName
-                  :io.jwt/token
-                  :urn.oid.1.2.840.113556.1.4.221
-                  :urn.oid.0.9.2342.19200300.100.1.3        ;; (email)
-                  :urn.oid.0.9.2342.19200300.100.1.1        ;; (uid)
-                  :urn.oid:2.5.4/givenName                  ;; (givenName)
-                  :urn.oid.2.5.4/surname                    ;; (sn)
-                  :urn.oid.2.5.4/title                      ;; job title, not prefix
-                  ]}
-  (let [wales-nadex (get-in login [:providers :com.eldrix.concierge/nadex])
-        fake-login (get-in login [:providers :com.eldrix.pc4/fake-login-provider])
-        token (make-user-token {:system system :value value} login)]
-    (cond
-      ;; do we have the NHS Wales' NADEX configured, and is it a namespace it can handle?
-      (and wales-nadex (= system "cymru.nhs.uk"))
-      (do
-        (log/info "login for " system value "; config:" wales-nadex)
-        (if-let [user (nadex/search (:connection-pool wales-nadex) value password)]
-          ;;(reduce-kv (fn [m k v] (assoc m (keyword "uk.nhs.cymru" (name k)) v)) {} user)
-          {:urn.oid.1.2.840.113556.1.4/sAMAccountName (:sAMAccountName user)
-           :io.jwt/token                              token
-           :urn.oid.2.5.4/surname                     (:sn user)
-           :urn.oid.2.5.4/givenName                   (:givenName user)
-           :urn.oid.2.5.4/title                       (:title user)}
-          (log/info "failed to authenticate user " system "/" value)))
-
-      ;; do we have a fake login provider configured?
-      fake-login
-      (do
-        (log/info "performing fake login for " system value password "expecting " (:username fake-login) (:password fake-login))
-        (when (and (= (:username fake-login) value) (= (:password fake-login) password))
-          (log/info "successful fake login")
-          {:urn.oid.1.2.840.113556.1.4/sAMAccountName value
-           :io.jwt/token                              token
-           :urn.oid.2.5.4/surname                     "Duck"
-           :urn.oid.2.5.4/givenName                   "Donald"
-           :urn.oid.2.5.4/title                       "Consultant Neurologist"}))
-      ;; no login provider found for the namespace provided
-      :else
-      (log/info "no login provider found for namespace" {:system system :providers (keys login)}))))
-
-(pco/defresolver x500->common-name
-  "Generates an x500 common-name."
-  [{:urn.oid.2.5.4/keys [givenName surname]}]
-  {::pco/output [:urn.oid.2.5.4/commonName]}                ;; (cn)   common name - first, middle, last
-  {:urn.oid.2.5.4/commonName (str givenName " " surname)})
-
-(pco/defresolver fhir-practitioner-name
-  "Generates a FHIR practitioner name from x500 data."
-  [{:urn.oid.2.5.4/keys [givenName surname]}]
-  {::pco/output [{:org.hl7.fhir.Practitioner/name [:org.hl7.fhir.HumanName/given
-                                                   :org.hl7.fhir.HumanName/family
-                                                   :org.hl7.fhir.HumanName/use]}]}
-  {:org.hl7.fhir.Practitioner/name {:org.hl7.fhir.HumanName/family givenName
-                                    :org.hl7.fhir.HumanName/given  surname
-                                    :org.hl7.fhir.HumanName/use    :org.hl7.fhir.name-use/usual}})
 
 (def default-resolvers
-  [login-operation
-   refresh-token-operation
-   x500->common-name
-   fhir-practitioner-name])
+  users/all-resolvers)
 
 (defmethod aero/reader 'ig/ref [_ _ value]
   (ig/ref value))
