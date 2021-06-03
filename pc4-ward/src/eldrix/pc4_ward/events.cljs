c0cf8be2834ad1e43ec632908e5f93326ec2baa4
Mark Wardle <mark@wardle.org>
2021-06-03 16:11:17 +0100

    Add regular server ping.

--- pc4-ward/src/eldrix/pc4_ward/events.cljs
+++ pc4-ward/src/eldrix/pc4_ward/events.cljs
@@ -18,12 +18,18 @@
 ;; call our timer events at the required intervals
 (defonce do-timer (do
                     ;;(js/setInterval #(rf/dispatch [::timer-one-second (js/Date.)]) 1000)
+                    (js/setInterval #(rf/dispatch [::time-10-seconds]) 10000)
                     (js/setInterval #(rf/dispatch [::timer-one-minute]) 60000)))
 
 (rf/reg-event-db                                            ;; usage:  (rf/dispatch [:timer a-js-Date])
   ::timer-one-second
   (fn [db [_ new-time]]                                     ;; <-- notice how we de-structure the event vector
     (assoc db :current-time new-time)))
+
+(rf/reg-event-fx
+  ::time-10-seconds
+  (fn [{db :db} [_]]
+    {:fx [[:dispatch [::users/ping-server]]]}))
 
 ;; every minute, we check our authentication tokens are valid, and refresh if necessary
 ;; we cannot refresh an expired login token ourselves, so give up and logout with a session expired notice
