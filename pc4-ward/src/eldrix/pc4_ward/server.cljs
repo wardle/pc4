eb83c6f6c2d4bd21d0115c40642a4d25f3377ec1
Mark Wardle <mark@wardle.org>
2021-06-03 16:12:24 +0100

    Update server.cljs

--- pc4-ward/src/eldrix/pc4_ward/server.cljs
+++ pc4-ward/src/eldrix/pc4_ward/server.cljs
@@ -1,7 +1,17 @@
 (ns eldrix.pc4-ward.server
+  "Support for interacting with the pc4 server.
+  The pc4 server provides a single HTTP POST API endpoint which accepts EQL.
+  This means it can be used to perform graph queries or execute operations
+  with the client deciding what properties to return.
+
+  There are two main methods for invoking this API. The first is a direct
+  call using cljs-ajax. The second is to use re-frame's http-fx handler.
+  Both have the same result."
+  (:require-macros [cljs.core.async.macros :refer [go]])
   (:require [clojure.string :as str]
             [ajax.core :as ajax]
-            [goog.crypt.base64 :as b64])
+            [goog.crypt.base64 :as b64]
+            [cljs.core.async :refer [<!]])
   (:import [goog.date UtcDateTime]))
 
 (defn jwt-token-payload
@@ -43,16 +53,14 @@
       (> now exp))))
 
 (defn make-xhrio-request
-  [{:keys [token params on-success on-failure]}]
-  {:method          :post
-   :uri             "http://localhost:8080/api"
-   :timeout         3000
-   :format          (ajax/transit-request-format)
-   :response-format (ajax/transit-response-format)
-   :headers         (when token {:Authorization (str "Bearer " token)})
-   :params          params
-   :on-success      on-success
-   :on-failure      on-failure})
+  [{:keys [token timeout _params _on-success _on-failure] :as opts :or {timeout 3000}}]
+  (merge {:method          :post
+          :uri             "http://localhost:8080/api"
+          :timeout         timeout
+          :format          (ajax/transit-request-format)
+          :response-format (ajax/transit-response-format)
+          :headers         (when token {:Authorization (str "Bearer " token)})}
+         opts))
 
 (defn make-snomed-search-op
   [{:keys [_s _constraint] :as params}]
@@ -69,6 +77,12 @@
           {:token token})
     [:io.jwt/token]}])
 
+(defn make-ping-op
+  [{:keys [uuid]}]
+  [{(list 'pc4.users/ping
+          {:uuid uuid})
+    [:uuid]}])
+
 (defn make-login-op
   [{:keys [system value password] :as params}]
   [{(list 'pc4.users/login
@@ -81,8 +95,27 @@
      :urn.oid.2.5.4/commonName
      :urn.oid.2.5.4/title
      :urn.oid.2.5.4/telephoneNumber
+     :org.hl7.fhir.Practitioner/telecom
      :org.hl7.fhir.Practitioner/identifier
      {:org.hl7.fhir.Practitioner/name
       [:org.hl7.fhir.HumanName/use
        :org.hl7.fhir.HumanName/family
        :org.hl7.fhir.HumanName/given]}]}])
+
+(defn do!
+  "Execute a xhrio request on the server."
+  [{:keys [request handler error-handler] :as opts}]
+  (ajax/POST "http://localhost:8080/api" (make-xhrio-request opts)))
+
+(defn do-snomed-search [{:keys [_s _constrant _max-hits]} {:keys [handler error-handler]}])
+
+(comment
+  (shadow.cljs.devtools.api/nrepl-select :app)
+  (make-login-op {:system "cymru.nhs.uk" :value "ma090906" :password "password"})
+
+  (def results (atom nil))
+  (do! {:params        (make-snomed-search-op {:s "amyloid" :max-hits 5})
+        :handler       #(reset! results %)
+        :error-handler #(println "failure: " %)})
+  @results
+  )
