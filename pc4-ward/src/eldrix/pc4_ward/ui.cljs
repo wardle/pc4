c014ca12921a72c8f520a3119b4fa62beafb1b05
Mark Wardle <mark@wardle.org>
2021-06-19 10:17:40 +0100

    Ensure user menu appears in front of everything else.

--- pc4-ward/src/eldrix/pc4_ward/ui.cljs
+++ pc4-ward/src/eldrix/pc4_ward/ui.cljs
@@ -80,7 +80,8 @@
                [:span.sm:hidden.text-white [:div.flex (when initials initials) [icon-chevron-down]]]
                (when photo [:img.h-8.w-8.rounded-full {:src "https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" :alt ""}])]]
              (when @show-user-menu?
-               [:div.origin-top-right.absolute.right-0.mt-2.w-48.rounded-md.shadow-lg.py-1.bg-white.ring-1.ring-black.ring-opacity-5.focus:outline-none {:role "menu" :aria-orientation "vertical" :aria-labelledby "user-menu-button" :tabIndex "-1"}
+               [:div.origin-top-right.absolute.z-50.right-0.mt-2.w-48.rounded-md.shadow-lg.py-1.bg-white.ring-1.ring-black.ring-opacity-5.focus:outline-none
+                {:role "menu" :aria-orientation "vertical" :aria-labelledby "user-menu-button" :tabIndex "-1"}
                 (for [item user-menu]
                   (if (:on-click item)
                     [:a.block.px-4.py-2.text-sm.text-gray-700.hover:bg-gray-700.hover:text-white
