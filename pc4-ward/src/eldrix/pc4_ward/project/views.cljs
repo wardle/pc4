f54b059b384c0e06dea2ab4376ba1903db135007
Mark Wardle <mark@wardle.org>
2021-10-26 16:49:52 +0100

    Start work on project page.

--- pc4-ward/src/eldrix/pc4_ward/project/views.cljs
+++ pc4-ward/src/eldrix/pc4_ward/project/views.cljs
@@ -1,11 +1,26 @@
 (ns eldrix.pc4-ward.project.views
   (:require [reitit.frontend.easy :as rfe]
-            [re-frame.core :as rf]))
+            [re-frame.core :as rf]
+            [eldrix.pc4-ward.project.subs :as project-subs]
+            [eldrix.pc4-ward.user.subs :as user-subs]
+            [eldrix.pc4-ward.user.events :as user-events]
+            [eldrix.pc4-ward.ui :as ui]
+            [clojure.string :as str]))
 
 
 (defn project-home-page []
-  (let [route @(rf/subscribe [:eldrix.pc4-ward.subs/current-route])
-        project-id (get-in route [:path-params :id])
-        project-slug (get-in route [:path-params :slug])]
-    [:div.grid.grid-cols-1.md:grid-cols-4.md:gap-4.m-4
-     [:div.col-span1.md:col-span-4 "Information about project: id:" project-id " slug: "project-slug]]))
+  (let [selected-page (reagent.core/atom :home)]
+    (fn []
+      (let [route @(rf/subscribe [:eldrix.pc4-ward.subs/current-route])
+            authenticated-user @(rf/subscribe [::user-subs/authenticated-user])
+            current-project @(rf/subscribe [::project-subs/current])]
+        (when current-project
+          [:div.grid.grid-cols-1.border-2.shadow-lg.p-1.sm:p-4.sm:m-2.border-gray-200
+
+           [:div.font-bold.text-lg.min-w-min (:t_project/title current-project)]
+           [:ul.flex
+            [ui/flat-menu [{:title "Home" :id :home}
+                           {:title "Register patient" :id :register-patient}
+                           {:title "Patients" :id :list-patients}]
+             :selected-id @selected-page
+             :select-fn #(do (println "selecting page " %) (reset! selected-page %))]]])))))
