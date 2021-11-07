297952e6ea3a27f0613356ee4d181061455141f4
Mark Wardle <mark@wardle.org>
2021-11-07 13:53:56 +0000

    Add create and update diagnosis.
    This legacy diagnostic list will be replaced by a per-encounter list, and the master diagnostic list will be updated from those instead.

--- pc4-server/src/com/eldrix/pc4/server/rsdb/patients.clj
+++ pc4-server/src/com/eldrix/pc4/server/rsdb/patients.clj
@@ -98,13 +98,47 @@
     #{}
     (jdbc/plan conn (patient-pks-on-medications-sql medication-concept-ids))))
 
+
+(defn create-diagnosis [conn {:t_diagnosis/keys  [concept_fk date_onset date_onset_accuracy date_diagnosis date_diagnosis_accuracy date_to date_to_accuracy status]
+                              patient-identifier :t_patient/patient_identifier}]
+  (db/execute-one! conn
+                   (sql/format {:insert-into [:t_diagnosis]
+                                     :values      [{:date_onset              date_onset
+                                                    :date_onset_accuracy     date_onset_accuracy
+                                                    :date_diagnosis          date_diagnosis
+                                                    :date_diagnosis_accuracy date_diagnosis_accuracy
+                                                    :date_to                 date_to
+                                                    :date_to_accuracy        date_to_accuracy
+                                                    :status                  (name status)
+                                                    :concept_fk              concept_fk
+                                                    :patient_fk              {:select :t_patient/id
+                                                                              :from   [:t_patient]
+                                                                              :where  [:= :t_patient/patient_identifier patient-identifier]}}]})
+                   {:return-keys true}))
+
+(defn update-diagnosis
+  [conn {:t_diagnosis/keys [concept_fk id date_onset date_onset_accuracy date_diagnosis date_diagnosis_accuracy date_to date_to_accuracy status]}]
+  (db/execute-one! conn
+                   (sql/format {:update [:t_diagnosis]
+                                :set    {:date_onset              date_onset
+                                         :date_onset_accuracy     date_onset_accuracy
+                                         :date_diagnosis          date_diagnosis
+                                         :date_diagnosis_accuracy date_diagnosis_accuracy
+                                         :date_to                 date_to
+                                         :date_to_accuracy        date_to_accuracy
+                                         :status                  (name status)}
+                                :where  [:and
+                                         [:= :t_diagnosis/concept_fk concept_fk]
+                                         [:= :t_diagnosis/id id]]})
+                   {:return-keys true}))
+
 (comment
   (patients-in-projects-sql [1 3 32] (LocalDate/now))
   (patients-in-projects conn #{1 3 32})
   (require '[next.jdbc.connection])
   (def conn (next.jdbc.connection/->pool com.zaxxer.hikari.HikariDataSource {:dbtype          "postgresql"
                                                                              :dbname          "rsdb"
-                                                                             :maximumPoolSize 10}))
+                                                                             :maximumPoolSize 2}))
   (fetch-episodes conn 15203)
   (active-episodes conn 15203)
   (active-project-identifiers conn 15203)
