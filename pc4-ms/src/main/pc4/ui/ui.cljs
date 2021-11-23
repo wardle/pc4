b39f2003637110922ccb7903f5a6a51121ec1588
Mark Wardle <mark@wardle.org>
2021-11-23 13:41:04 +0000

    Add quick hack date formatting

--- pc4-ms/src/main/pc4/ui/ui.cljs
+++ pc4-ms/src/main/pc4/ui/ui.cljs
@@ -1,7 +1,30 @@
 (ns pc4.ui.ui
   (:require
     [com.fulcrologic.fulcro.components :as comp :refer [defsc]]
-    [com.fulcrologic.fulcro.dom :as dom]))
+    [com.fulcrologic.fulcro.dom :as dom])
+  (:import [goog.date Date]))
+
+(def months-en
+  {0  "Jan"
+   1  "Feb"
+   2  "Mar"
+   3  "Apr"
+   4  "May"
+   5  "Jun"
+   6  "Jul"
+   7  "Aug"
+   8  "Sep"
+   9  "Oct"
+   10 "Nov"
+   11 "Dec"})
+
+(defn format-date [^Date date]
+  (when date (str (.getDate date)
+                  "-"
+                  (get months-en (.getMonth date))
+                  "-"
+                  (.getYear date))))
+
 
 (defn box-error-message [& {:keys [title message]}]
   (when message
