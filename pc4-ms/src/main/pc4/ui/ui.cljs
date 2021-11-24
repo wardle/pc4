f1ea81fd93559946f48819dc4dfc783417eab8ef
Mark Wardle <mark@wardle.org>
2021-11-24 20:43:56 +0000

    Tweaks/fixes to nav bar

--- pc4-ms/src/main/pc4/ui/ui.cljs
+++ pc4-ms/src/main/pc4/ui/ui.cljs
@@ -1,4 +1,5 @@
 (ns pc4.ui.ui
+  "Stateless plain components."
   (:require
     [com.fulcrologic.fulcro.components :as comp :refer [defsc]]
     [com.fulcrologic.fulcro.dom :as dom :refer [a button div img path span svg nav]])
@@ -27,8 +28,7 @@
 
 (defn icon-chevron-down []
   (svg :.-mr-1.ml-2.h-5.w-5 {:xmlns "http://www.w3.org/2000/svg" :viewBox "0 0 20 20" :fill "white" :aria-hidden "true"}
-       (path {:fill-rule "evenodd" :d "M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" :clip-rule "evenodd"})))
-
+       (path {:fillRule "evenodd" :d "M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" :clipRule "evenodd"})))
 
 (defn box-error-message [& {:keys [title message]}]
   (when message
@@ -43,11 +43,11 @@
   - menu       : the main menu, a sequence of menu items with
                 |- :id       : unique identifier
                 |- :title    : the menu item title
-                |- :on-click : function for when clicked
+                |- :onClick : function for when clicked
                 |- :href     : href for when clicked
   - selected   : identifier of the currently selected menu
   - show-user? : should the user and user menu be shown?
-  - user-menu  : a sequence of menu items each with keys :id :title :on-click
+  - user-menu  : a sequence of menu items each with keys :id :title :onClick
   - full-name  : full name of user
   - initials   : initials of user (for display on small devices)
   - photo      : photo to show for user  [todo: URL or something else?]
@@ -62,8 +62,8 @@
                    (when (seq menu)
                      (div :.absolute.inset-y-0.left-0.flex.items-center.sm:hidden
                           (button :.inline-flex.items-center.justify-center.p-2.rounded-md.text-gray-400.hover:text-white.hover:bg-gray-700.focus:ring-2.focus:ring-inset.focus:ring-white
-                                  {:type     "button" :aria-controls "mobile-menu" :aria-expanded "false"
-                                   :on-click #(comp/set-state! this {:show-menu (not show-menu?)})}
+                                  {:type    "button" :aria-controls "mobile-menu" :aria-expanded "false"
+                                   :onClick #(comp/set-state! this {:show-menu (not show-menu?)})}
                                   (span :.sr-only "Open main menu")
                                   (svg :.block.h-6.w-6 {:xmlns "http://www.w3.org/2000/svg" :fill "none" :viewBox "0 0 24 24" :stroke "currentColor" :aria-hidden "true"}
                                        (path {:stroke-linecap "round" :stroke-linejoin "round" :stroke-width "2" :d "M4 6h16M4 12h16M4 18h16"}))
@@ -77,18 +77,18 @@
                                (div :.flex.space-x-4
                                     (for [item menu]
                                       (if (and selected (:id item) (= (:id item) selected))
-                                        (a :.text-white.px-3.py-2.rounded-md.text-sm.font-medium.font-bold {:key (:id item) :on-click (:on-click item) :aria-current "page"} (:title item))
+                                        (a :.text-white.px-3.py-2.rounded-md.text-sm.font-medium.font-bold {:key (:id item) :onClick (:onClick item) :aria-current "page"} (:title item))
                                         (a :.text-gray-300.hover:bg-gray-700.hover:text-white.px-3.py-2.rounded-md.text-sm.font-medium
                                            (cond-> {:key (:id item)}
                                                    (:href item) (assoc :href (:href item))
-                                                   (:on-click item) (assoc :onClick (:on-click item)))
+                                                   (:onClick item) (assoc :onClick (:onClick item)))
                                            (:title item))))))))
                    (div :.absolute.inset-y-0.right-0.flex.items-center.pr-2.sm:static.sm:inset-auto.sm:ml-6.sm:pr-0
                         (when show-user?
                           (div :.ml-3.relative
                                (div
                                  (button :.user-menu-button.bg-gray-800.flex.text-sm.rounded-full
-                                         {:type     "button" :aria-expanded "false" :aria-haspopup "true"
+                                         {:type    "button" :aria-expanded "false" :aria-haspopup "true"
                                           :onClick #(comp/set-state! this {:show-user-menu (not show-user-menu?)})}
                                          (span :.sr-only "Open user menu")
                                          (span :.hidden.sm:block.text-white (div :.flex (or full-name "User") (icon-chevron-down)))
@@ -98,12 +98,10 @@
                                  (div :.origin-top-right.absolute.z-50.right-0.mt-2.w-48.rounded-md.shadow-lg.py-1.bg-white.ring-1.ring-black.ring-opacity-5.focus:outline-none
                                       {:role "menu" :aria-orientation "vertical" :aria-labelledby "user-menu-button" :tabIndex "-1"}
                                       (for [item user-menu]
-                                        (if (:on-click item)
-                                          (a :.block.px-4.py-2.text-sm.text-gray-700.hover:bg-gray-700.hover:text-white
-                                             {:key (:id item) :onClick #(do
-                                                                          (println "clicked menu")
-                                                                          (comp/set-state! this {:show-user-menu (not show-user-menu?)})
-                                                                          ((:on-click item))) :role "menuitem" :tabIndex "-1"} (:title item))
+                                        (if (:onClick item)
+                                          (a :.block.px-4.py-2.text-sm.text-gray-700.hover:bg-gray-700.hover:text-white.hover:cursor-pointer
+                                             {:key (:id item) :onClick #(do (comp/set-state! this {:show-user-menu (not show-user-menu?)})
+                                                                            ((:onClick item))) :role "menuitem" :tabIndex "-1"} (:title item))
                                           (a :.block.px-4.py-2.text-sm.text-gray-700.italic {:key (:id item) :role "menuitem" :tabIndex "-1"} (:title item))))))))))
               (when (and (seq menu) show-menu?)
                 (div :.mobile-menu.sm:hidden
@@ -111,9 +109,9 @@
                           (for [item menu]
                             (if (and selected (:id item) (= (:id item) selected))
                               (a :.bg-gray-900.text-white.block.px-3.py-2.rounded-md.text-base.font-medium
-                                 {:key (:id item) :on-click (:on-click item) :aria-current "page"} (:title item))
+                                 {:key (:id item) :onClick (:onClick item) :aria-current "page"} (:title item))
                               (a :.text-gray-300.hover:bg-gray-700.hover:text-white.block.px-3.py-2.rounded-md.text-base.font-medium
-                                 {:key (:id item) :on-click (:on-click item)} (:title item)))))))))))
+                                 {:key (:id item) :onClick (:onClick item)} (:title item)))))))))))
 
 
 (def ui-nav-bar (comp/factory nav-bar))
