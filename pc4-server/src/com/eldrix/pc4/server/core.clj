5fb4f5946b33727c5b81031da9e35d0effb2c108
Mark Wardle <mark@wardle.org>
2021-11-26 20:19:36 +0000

    Use pedestal for now

--- pc4-server/src/com/eldrix/pc4/server/core.clj
+++ pc4-server/src/com/eldrix/pc4/server/core.clj
@@ -1,7 +1,11 @@
 (ns com.eldrix.pc4.server.core
   (:require [clojure.tools.logging :as log]
-            [com.eldrix.pc4.server.system :as pc4]))
+            [com.eldrix.pc4.server.system :as pc4]
+            [clojure.string :as str]))
 
-(defn run [{:keys [profile] :or {profile :live}}]
+(defn run [{:keys [profile]}]
+  (when (str/blank? profile)
+    (log/error "Missing :profile")
+    (System/exit 0))
   (log/info "starting pc4-server with profile" {:profile profile})
-  (pc4/init profile [:http/server2]))
+  (pc4/init profile [:http/server]))
