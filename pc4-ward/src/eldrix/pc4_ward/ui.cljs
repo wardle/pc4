16b50bb6f96c197d47ff1d0ea7e91e88cbb8eb7a
Mark Wardle <mark@wardle.org>
2021-11-27 12:16:11 +0000

    Put login controls in a form

--- pc4-ward/src/eldrix/pc4_ward/ui.cljs
+++ pc4-ward/src/eldrix/pc4_ward/ui.cljs
@@ -64,6 +64,7 @@
     (fn [& {:keys [on-login disabled error] :or {disabled false}}]
       [:div.flex.items-center.justify-center.bg-gray-50.py-12.px-4.sm:px-6.lg:px-8
        [:div.max-w-md.w-full.space-y-8
+        [:form {:on-submit #(.preventDefault %)}
         [:div
          [:h1.mx-auto.w-auto.text-center.text-4xl.text-indigo-700.tracking-tighter "PatientCare " [:span.font-bold "v4"]]]
         [:div.rounded-md.shadow-sm.-space-y-px
@@ -88,7 +89,7 @@
           {:type "submit" :on-click #(when on-login (on-login @username @password)) :disabled disabled :class (if disabled "bg-gray-400 animate-pulse" "bg-indigo-600 hover:bg-indigo-700")}
           [:span.absolute.left-0.inset-y-0.flex.items-center.pl-3
            [:svg.h-5.w-5.text-indigo-500.group-hover:text-indigo-400 {:xmlns "http://www.w3.org/2000/svg" :viewBox "0 0 20 20" :fill "currentColor" :aria-hidden "true" :class (when disabled "animate-bounce")}
-            [:path {:fill-rule "evenodd" :d "M5 9V7a5 5 0 0110 0v2a2 2 0 012 2v5a2 2 0 01-2 2H5a2 2 0 01-2-2v-5a2 2 0 012-2zm8-2v2H7V7a3 3 0 016 0z" :clip-rule "evenodd"}]]] (if disabled "Signing in" "Sign in")]]]])))
+            [:path {:fill-rule "evenodd" :d "M5 9V7a5 5 0 0110 0v2a2 2 0 012 2v5a2 2 0 01-2 2H5a2 2 0 01-2-2v-5a2 2 0 012-2zm8-2v2H7V7a3 3 0 016 0z" :clip-rule "evenodd"}]]] (if disabled "Signing in" "Sign in")]]]]])))
 
 
 (defn ui-textfield [& {:keys [name autocomplete field-type error] :or {field-type "text"}}]
