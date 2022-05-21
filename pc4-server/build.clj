5513bf98a4c145b81f2b98e1255c4f23c453315d
Mark Wardle <mark@wardle.org>
2022-05-21 10:39:31 +0100

    Build a library jar, not an uberjar

--- pc4-server/build.clj
+++ pc4-server/build.clj
@@ -6,31 +6,29 @@
 (def version (format "1.0.%s" (b/git-count-revs nil)))
 (def class-dir "target/classes")
 (def basis (b/create-basis {:project "deps.edn"}))
-(def jar-file (format "target/%s-%s.jar" (name lib) version))
+(def jar-file (format "target/%s-lib-%s.jar" (name lib) version))
+(def uber-file (format "target/%s-%s.jar" (name lib) version))
 
 (defn clean [_]
       (b/delete {:path "target"}))
 
 (defn jar [_]
       (clean nil)
-      (println "Building   :" lib version)
-      (b/compile-clj {:basis      basis
-                      :class-dir  class-dir
-                      :src-dir    ["src"]
-                      :ns-compile ['com.eldrix.pc4.server.system]})
+      (println "Building" jar-file)
       (b/write-pom {:class-dir class-dir
                     :lib       lib
                     :version   version
                     :basis     basis
                     :src-dirs  ["src"]
-                    :scm       {:url                 "https://github.com/wardle/pc4"
+                    :scm       {:url                 "https://github.com/wardle/hermes"
                                 :tag                 (str "v" version)
-                                :connection          "scm:git:git://github.com/wardle/pc4.git"
-                                :developerConnection "scm:git:ssh://git@github.com/wardle/pc4.git"}})
+                                :connection          "scm:git:git://github.com/wardle/hermes.git"
+                                :developerConnection "scm:git:ssh://git@github.com/wardle/hermes.git"}})
       (b/copy-dir {:src-dirs   ["src" "resources"]
                    :target-dir class-dir})
       (b/jar {:class-dir class-dir
               :jar-file  jar-file}))
+
 
 (defn install
       "Installs pom and library jar in local maven repository"
@@ -42,3 +40,4 @@
                   :class-dir class-dir
                   :version   version
                   :jar-file  jar-file}))
+
