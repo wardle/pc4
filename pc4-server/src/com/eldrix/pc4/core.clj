4ceba76b51aa21227fc74a0db5ff4a513dc11d8b
Mark Wardle <mark@wardle.org>
2022-07-07 01:36:22 +0100

    Fix namespaces to load / initialise

--- pc4-server/src/com/eldrix/pc4/core.clj
+++ pc4-server/src/com/eldrix/pc4/core.clj
@@ -1,11 +1,17 @@
 (ns com.eldrix.pc4.core
   (:require [clojure.tools.logging :as log]
-            [com.eldrix.pc4.system :as pc4]))
+            [com.eldrix.pc4.system :as pc4])
+  (:gen-class))
 
 (defn run [{:keys [profile]}]
   (when-not profile
     (log/error "Missing :profile")
     (System/exit 0))
   (log/info "starting pc4-server with profile" {:profile profile})
-  (pc4/load-namespaces profile [:com.eldrix.pc4.server/pedestal])
-  (pc4/init profile [:com.eldrix.pc4.server/pedestal]))
+  (pc4/load-namespaces profile [:com.eldrix.pc4.pedestal/server])
+  (pc4/init profile [:com.eldrix.pc4.pedestal/server]))
+
+(defn -main [& args]
+  (if-let [profile (first args)]
+    (run {:profile (keyword profile)})
+    (println "Missing parameter 'profile' e.g. cvx, pc4 or dev")))
