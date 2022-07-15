be32e9bc6a2190188f605a436c1defbca66037a8
Mark Wardle <mark@wardle.org>
2022-07-15 21:14:57 +0100

    Add user photograph to navigation bar, if available

--- pc4-ms/src/main/pc4/ui.cljs
+++ pc4-ms/src/main/pc4/ui.cljs
@@ -78,9 +78,6 @@
 
 
 
-
-
-
 (defsc nav-bar
   "A stateless navigation bar.
   Parameters:
@@ -95,7 +92,7 @@
   - user-menu  : a sequence of menu items each with keys :id :title :onClick
   - full-name  : full name of user
   - initials   : initials of user (for display on small devices)
-  - photo      : photo to show for user  [todo: URL or something else?]
+  - photo      : URL of photo to show for user
   - on-home    : function if home button clicked
   - on-notify  : function if notify button should appear (and call if clicked)"
   [this
@@ -136,24 +133,26 @@
                                            (:title item))))))))
                    (div :.absolute.inset-y-0.right-0.flex.items-center.pr-2.sm:static.sm:inset-auto.sm:ml-6.sm:pr-0
                         (when show-user?
-                          (div :.ml-3.relative
-                               (div
-                                 (button :.user-menu-button.bg-gray-800.flex.text-sm.rounded-full
-                                         {:type    "button" :aria-expanded "false" :aria-haspopup "true"
-                                          :onClick #(comp/set-state! this {:show-user-menu (not show-user-menu?)})}
-                                         (span :.sr-only "Open user menu")
-                                         (span :.hidden.sm:block.text-white (div :.flex (or full-name "User") (icon-chevron-down)))
-                                         (span :.sm:hidden.text-white (div :.flex (when initials initials) (icon-chevron-down)))
-                                         (when photo (img :.h-8.w-8.rounded-full {:src "https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" :alt ""}))))
-                               (when show-user-menu?
-                                 (div :.origin-top-right.absolute.z-50.right-0.mt-2.w-48.rounded-md.shadow-lg.py-1.bg-white.ring-1.ring-black.ring-opacity-5.focus:outline-none
-                                      {:role "menu" :aria-orientation "vertical" :aria-labelledby "user-menu-button" :tabIndex "-1"}
-                                      (for [item user-menu]
-                                        (if (:onClick item)
-                                          (a :.block.px-4.py-2.text-sm.text-gray-700.hover:bg-gray-700.hover:text-white.cursor-pointer
-                                             {:key (:id item) :onClick #(do (comp/set-state! this {:show-user-menu (not show-user-menu?)})
-                                                                            ((:onClick item))) :role "menuitem" :tabIndex "-1"} (:title item))
-                                          (a :.block.px-4.py-2.text-sm.text-gray-700.italic {:key (:id item) :role "menuitem" :tabIndex "-1"} (:title item))))))))))
+                          (comp/fragment
+                            (when photo
+                              (div (img :.inline-block.h-10.w-10.rounded-md {:src photo :alt ""})))
+                            (div :.ml-3.relative
+                                 (div
+                                   (button :.user-menu-button.bg-gray-800.flex.text-sm.rounded-full
+                                           {:type    "button" :aria-expanded "false" :aria-haspopup "true"
+                                            :onClick #(comp/set-state! this {:show-user-menu (not show-user-menu?)})}
+                                           (span :.sr-only "Open user menu")
+                                           (span :.hidden.sm:block.text-white (div :.flex (or full-name "User") (icon-chevron-down)))
+                                           (span :.sm:hidden.text-white (div :.flex (when initials initials) (icon-chevron-down)))))
+                                 (when show-user-menu?
+                                   (div :.origin-top-right.absolute.z-50.right-0.mt-2.w-48.rounded-md.shadow-lg.py-1.bg-white.ring-1.ring-black.ring-opacity-5.focus:outline-none
+                                        {:role "menu" :aria-orientation "vertical" :aria-labelledby "user-menu-button" :tabIndex "-1"}
+                                        (for [item user-menu]
+                                          (if (:onClick item)
+                                            (a :.block.px-4.py-2.text-sm.text-gray-700.hover:bg-gray-700.hover:text-white.cursor-pointer
+                                               {:key (:id item) :onClick #(do (comp/set-state! this {:show-user-menu (not show-user-menu?)})
+                                                                              ((:onClick item))) :role "menuitem" :tabIndex "-1"} (:title item))
+                                            (a :.block.px-4.py-2.text-sm.text-gray-700.italic {:key (:id item) :role "menuitem" :tabIndex "-1"} (:title item)))))))))))
               (when (and (seq menu) show-menu?)
                 (div :.mobile-menu.sm:hidden
                      (div :.px-2.pt-2.pb-3.space-y-1
