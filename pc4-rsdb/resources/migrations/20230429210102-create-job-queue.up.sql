f17a4d698d6db8dd04636cf78aa7d4d5995bf3fa
Mark Wardle <mark@wardle.org>
2023-04-30 13:25:27 +0100

    wip job queue

--- pc4-rsdb/resources/migrations/20230429210102-create-job-queue.up.sql
+++ pc4-rsdb/resources/migrations/20230429210102-create-job-queue.up.sql
@@ -1,6 +1,10 @@
 CREATE TABLE t_job_queue (
     id int not null primary key generated always as identity,
     topic text not null,
-    queue_time	timestamptz default now(),
+    created timestamptz default now(),
     payload	text
 );
+--;;
+CREATE INDEX t_job_queue_topic_idx on t_job_queue(topic);
+--;;
+CREATE INDEX t_job_queue_created_time_ids on t_job_queue(created)
--- pc4-rsdb/src/com/eldrix/pc4/rsdb/job.clj
+++ pc4-rsdb/src/com/eldrix/pc4/rsdb/job.clj
@@ -1,55 +1,79 @@
 (ns com.eldrix.pc4.rsdb.job
-  "Queue for asynchronous jobs built using PostgreSQL"
+  "Queue for asynchronous jobs built using PostgreSQL."
   (:require [clojure.edn :as edn]
+            [clojure.spec.alpha :as s]
             [clojure.tools.logging.readable :as log]
             [next.jdbc :as jdbc]
             [honey.sql :as sql]))
 
-(defn get-pending-jobs-sql
-  ([n] (get-pending-jobs-sql nil n))
+(defn pr-topic [kw]
+  (subs (str kw) 1))
+
+(defn ^:private pending-jobs-sql
+  ([n] (pending-jobs-sql nil n))
   ([topic n]
    (sql/format {:delete-from :t_job_queue
                 :using       [[(cond-> {:select   :*
                                         :from     :t_job_queue
                                         :limit    n
-                                        :order-by [[:queue_time :asc]]
+                                        :order-by [[:created :asc]]
                                         :for      [:update :skip-locked]}
-                                       topic (assoc :where [:= :topic topic]))
+                                       topic (assoc :where [:= :topic (pr-topic topic)]))
                                :q]]
                 :where       [:= :q/id :t_job_queue/id]
                 :returning   :t_job_queue/*})))
 
-(defn get-pending-jobs
+
+
+(s/fdef dequeue-jobs
+  :args (s/cat :txn any? :topic (s/? (s/nilable keyword?)) :n pos-int?))
+(defn dequeue-jobs
   "Return pending jobs, or nil. Perform in a transaction so that jobs
   remains queued until transaction complete."
-  ([txn n] (get-pending-jobs txn nil n))
+  ([txn n] (dequeue-jobs txn nil n))
   ([txn topic n]
-   (->> (jdbc/execute! txn (get-pending-jobs-sql topic n))
-        (map :t_job_queue/payload)
-        (map edn/read-string))))
+   (reduce (fn [acc {payload :payload}] (conj acc (edn/read-string payload)))
+           []
+           (jdbc/plan txn (pending-jobs-sql topic n)))))
 
-(defn queue-job [conn topic job]
+(s/fdef dequeue-job
+  :args (s/cat :txn any? :topic (s/? (s/nilable keyword?))))
+(defn dequeue-job
+  ([txn] (dequeue-job txn nil))
+  ([txn topic]
+   (edn/read-string (:t_job_queue/payload (jdbc/execute-one! txn (pending-jobs-sql topic 1))))))
+
+(s/fdef enqueue-job
+  :args (s/cat :conn any? :topic keyword? :job any?))
+(defn enqueue-job [conn topic job]
   (jdbc/execute! conn
                  (sql/format {:insert-into :t_job_queue
                               :columns     [:topic :payload]
-                              :values      [[topic (pr-str job)]]})))
+                              :values      [[(pr-topic topic) (pr-str job)]]})))
+
 
 (defn queue-stats
+  "Return a map of queue statistics keyed by topic. Currently, the only
+  statistic returned is :pending representing a count of pending work in that
+  topic. "
   [conn]
-  (reduce (fn [acc {topic :topic, n :count}] (assoc acc topic n))
+  (reduce (fn [acc {topic :topic, n :count}] (assoc acc (keyword topic) {:pending n}))
           {}
           (jdbc/plan conn (sql/format {:select   [:topic :%count.id]
                                        :from     :t_job_queue
                                        :group-by :topic}))))
 
 (comment
+  (require '[clojure.spec.test.alpha])
+  (clojure.spec.test.alpha/instrument)
   (def conn (jdbc/get-connection {:dbtype "postgresql" :dbname "rsdb"}))
-  (jdbc/execute! conn (get-pending-jobs-sql))
+  (jdbc/execute! conn (pending-jobs-sql 5))
   (jdbc/with-transaction [txn conn]
-    (when-let [jobs (seq (get-pending-jobs txn "message" 5))]
-      (log/info "Processing jobs" jobs)
-      (throw (ex-info "I failed to perform jobs" {:jobs jobs}))))
-  (dotimes [n 5] (queue-job conn "message" {:a n :download false}))
-  (get-pending-jobs conn "message" 5)
+    (when-let [job (dequeue-job txn :basic/report)]
+      (log/info "Processing job" job)
+      (throw (ex-info "I failed to perform job" {:job job}))))
+  (dotimes [n 5] (enqueue-job conn :user/message {:from_user_id 1 :t_user_id 5 :message "Hello"}))
+  (dequeue-job conn :user/message)
+  (dequeue-jobs conn 5)
   (queue-stats conn)
-  (jdbc/execute! conn ["vacuum full t_job_queue"]))
+  (jdbc/execute! conn ["vacuum full verbose t_job_queue"]))
