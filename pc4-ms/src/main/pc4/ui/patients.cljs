ed8fd297ad10bd72c098c87073f8351b440824ba
Mark Wardle <mark@wardle.org>
2023-07-24 20:36:24 +0100

    Ensure deceased banner doesn't overlap close button

--- pc4-ms/src/main/pc4/ui/patients.cljs
+++ pc4-ms/src/main/pc4/ui/patients.cljs
@@ -73,7 +73,7 @@
           (dom/svg {:xmlns "http://www.w3.org/2000/svg" :width "20" :height "20" :viewBox "0 0 18 18"}
                    (dom/path {:d "M14.53 4.53l-1.06-1.06L9 7.94 4.53 3.47 3.47 4.53 7.94 9l-4.47 4.47 1.06 1.06L9 10.06l4.47 4.47 1.06-1.06L10.06 9z"})))))
     (when deceased
-      (div :.grid.grid-cols-1.pb-2
+      (div :.grid.grid-cols-1.pb-2.mr-4
         (ui/ui-badge {:label (cond (instance? goog.date.Date deceased) (str "Died " (ui/format-date deceased))
                                    (string? deceased) (str "Died " deceased)
                                    :else "Deceased")})))
