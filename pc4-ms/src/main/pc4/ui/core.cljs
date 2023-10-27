0b69223f6722a96975240417595c90305665919b
Mark Wardle <mark@wardle.org>
2023-10-27 21:07:48 +0100

    Whitespace

--- pc4-ms/src/main/pc4/ui/core.cljs
+++ pc4-ms/src/main/pc4/ui/core.cljs
@@ -45,17 +45,17 @@
   [this {:keys [w h label]}]
   (let [label (or label (str w "x" h))]
     (dom/svg #js {:width w :height h}
-             (dom/rect #js {:width w :height h :style #js {:fill        "rgb(200,200,200)"
-                                                           :strokeWidth 2
-                                                           :stroke      "black"}})
-             (dom/text #js {:textAnchor "middle" :x (/ w 2) :y (/ h 2)} label))))
+      (dom/rect #js {:width w :height h :style #js {:fill        "rgb(200,200,200)"
+                                                    :strokeWidth 2
+                                                    :stroke      "black"}})
+      (dom/text #js {:textAnchor "middle" :x (/ w 2) :y (/ h 2)} label))))
 
 (def ui-placeholder (comp/factory PlaceholderImage))
 
 
 (defn icon-chevron-down []
   (svg :.-mr-1.ml-2.h-5.w-5 {:xmlns "http://www.w3.org/2000/svg" :viewBox "0 0 20 20" :fill "white" :aria-hidden "true"}
-       (path {:fillRule "evenodd" :d "M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" :clipRule "evenodd"})))
+    (path {:fillRule "evenodd" :d "M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" :clipRule "evenodd"})))
 
 (defn icon-exclamation [& {:keys [text]}]                   ;; from https://flowbite.com/icons/
   (svg :.w-6.h-6.text-gray-800.dark:text-white {:aria-hidden "true" :xmlns "http://www.w3.org/2000/svg" :fill "none" :viewBox "0 0 20 20"}
@@ -94,16 +94,16 @@
   (dom/div
     {:role "status"}
     (dom/svg :.mr-2.w-8.h-8.text-gray-200.animate-spin.dark:text-gray-600.fill-blue-600
-             {:aria-hidden "true",
-              :fill        "none",
-              :xmlns       "http://www.w3.org/2000/svg",
-              :viewBox     "0 0 100 101"}
-             (dom/path
-               {:d    "M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z",
-                :fill "currentColor"})
-             (dom/path
-               {:d    "M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z",
-                :fill "currentFill"}))
+      {:aria-hidden "true",
+       :fill        "none",
+       :xmlns       "http://www.w3.org/2000/svg",
+       :viewBox     "0 0 100 101"}
+      (dom/path
+        {:d    "M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z",
+         :fill "currentColor"})
+      (dom/path
+        {:d    "M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z",
+         :fill "currentFill"}))
     (dom/span :.sr-only "Loading...")))
 
 (def ui-loading (comp/factory UILoading))
@@ -310,7 +310,7 @@
 
 (defsc UIButton [this {:keys [key disabled? role onClick]}]
   (dom/button :.w-full.inline-flex.justify-center.rounded-md.border.shadow-sm.px-4.py-2.text-base.font-medium.focus:outline-none.focus:ring-2.focus:ring-offset-2.focus:ring-red-500.sm:ml-3.sm:w-auto.sm:text-sm
-    {:key key
+    {:key       key
      :type      "button"
      :className (case role :primary "border-transparent text-white bg-red-600" "bg-white")
      :classes   [(if disabled? "opacity-50" "cursor-pointer")]
