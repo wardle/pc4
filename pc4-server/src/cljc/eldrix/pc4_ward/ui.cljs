d4820d3b5934b344d0b38188b990af18389fe953
Mark Wardle <mark@wardle.org>
2023-11-18 13:40:09 +0000

    Trim whitespace from entered username

--- pc4-server/src/cljc/eldrix/pc4_ward/ui.cljs
+++ pc4-server/src/cljc/eldrix/pc4_ward/ui.cljs
@@ -87,7 +87,7 @@
            [box-error-message :message error])
          [:div.mt-2
           [:button.group.relative.w-full.flex.justify-center.py-2.px-4.border.border-transparent.text-sm.font-medium.rounded-md.text-white.focus:outline-none.focus:ring-2.focus:ring-offset-2.focus:ring-indigo-500
-           {:type "submit" :on-click #(when on-login (on-login @username @password)) :disabled disabled :class (if disabled "bg-gray-400 animate-pulse" "bg-indigo-600 hover:bg-indigo-700")}
+           {:type "submit" :on-click #(when on-login (on-login (str/trim @username) @password)) :disabled disabled :class (if disabled "bg-gray-400 animate-pulse" "bg-indigo-600 hover:bg-indigo-700")}
            [:span.absolute.left-0.inset-y-0.flex.items-center.pl-3
             [:svg.h-5.w-5.text-indigo-500.group-hover:text-indigo-400 {:xmlns "http://www.w3.org/2000/svg" :viewBox "0 0 20 20" :fill "currentColor" :aria-hidden "true" :class (when disabled "animate-bounce")}
              [:path {:fill-rule "evenodd" :d "M5 9V7a5 5 0 0110 0v2a2 2 0 012 2v5a2 2 0 01-2 2H5a2 2 0 01-2-2v-5a2 2 0 012-2zm8-2v2H7V7a3 3 0 016 0z" :clip-rule "evenodd"}]]] (if disabled "Signing in" "Sign in")]]]]])))
