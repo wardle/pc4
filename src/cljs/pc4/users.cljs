3156796894b3f9f8929c442ac3f8698b3ce17ecf
Mark Wardle <mark@wardle.org>
2023-12-30 10:10:36 +0000

    Tidy up data flow in login and logout

--- src/cljs/pc4/users.cljs
+++ src/cljs/pc4/users.cljs
@@ -5,18 +5,23 @@
     [com.fulcrologic.fulcro.dom :as dom :refer [div span li p]]
     [com.fulcrologic.fulcro.dom.events :as evt]
     [com.fulcrologic.fulcro.mutations :as m :refer [defmutation]]
-    [com.fulcrologic.fulcro.routing.dynamic-routing :as dr]))
+    [com.fulcrologic.fulcro.routing.dynamic-routing :as dr]
+    [taoensso.timbre :as log]))
 
+
+(defn update-login-state*
+  [state {:keys [error loading]}]
+  (-> state
+      (assoc-in [:component/id :login :ui/error] error)
+      (assoc-in [:component/id :login :ui/loading?] loading)))
 
 (defmutation perform-login
   "Performs a login action. If the server responds successfully (200) but there
-  is no token, then we have invalid credentials."
+  is no user, then we have invalid credentials."
   [params]
   (action [{:keys [state]}]
-          (js/console.log "Performing login action" (assoc params :password "******"))
-          (swap! state #(-> %
-                            (update-in [:component/id :login] dissoc :ui/error)
-                            (assoc-in [:component/id :login :ui/loading?] true))))
+          (log/info "Performing login action" (assoc params :password "******"))
+          (swap! state update-login-state* {:error nil :loading false}))
 
   (login [env]
          (js/console.log "Sending login action to remote" env)
@@ -24,11 +29,9 @@
              (m/returning 'pc4.ui.users/UserHomePage)
              (m/with-target [:session/authenticated-user])))
   (ok-action [{:keys [result state app] :as env}]
-             (swap! state assoc-in [:component/id :login :ui/loading?] false)
-             (let [token (get-in result [:body 'pc4.users/perform-login :io.jwt/token])]
-               (js/console.log "response from remote: " result)
-               (if token (swap! state update-in [:component/id :login] dissoc :ui/error)
-                         (swap! state assoc-in [:component/id :login :ui/error] "Invalid username or password"))
+             (let [user-id (get-in result [:body 'pc4.users/perform-login :t_user/id])]
+               (log/debug "response from remote: " result)
+               (swap! state update-login-state* {:error (when-not user-id "Invalid username or password") :loading false})
                (dr/change-route! app ["home"])))
   (error-action [{:keys [state] :as env}]
                 (swap! state #(-> %
@@ -37,13 +40,15 @@
 
 (defmutation logout
   [{:keys [message]}]
-  (remote [_] true)
   (ok-action [{:keys [app state]}]
-             (js/console.log "Performing logout action" message)
-             (dr/change-route! app ["home"])
-             (swap! state (fn [s]
-                            (cond-> (assoc s :session/authenticated-user {})
-                                    message
-                                    (assoc-in [:component/id :login :ui/error] message))))
-             (.reload js/window.location true)))
+          (js/console.log "Performing logout action" message)
+          (swap! state (fn [s]
+                         (cond-> (assoc s :session/authenticated-user {})
+                                 message
+                                 (assoc-in [:component/id :login :ui/error] message))))
+          (dr/change-route! app ["login"])
+          (.reload js/window.location true))
+  (error-action [_]
+    (.reload js/window.location true))
+  (remote [_] true))
 
