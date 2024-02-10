f49435e63025481ed41da6cb55d5f3de9ed10ff9
Mark Wardle <mark@wardle.org>
2024-02-10 10:35:20 +0000

    Bump version

--- build.clj
+++ build.clj
@@ -4,7 +4,7 @@
             [deps-deploy.deps-deploy :as dd]))
 
 (def lib 'com.eldrix/pc4)
-(def version (format "1.0.%s" (b/git-count-revs nil)))
+(def version (format "1.1.%s" (b/git-count-revs nil)))
 (def class-dir "target/classes")
 (def basis (b/create-basis {:project "deps.edn"}))
 (def jar-file (format "target/%s-lib-%s.jar" (name lib) version))
