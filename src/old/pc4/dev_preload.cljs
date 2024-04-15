4d9a98dec4c5519072b3e58021d3f3eb8d44363e
Mark Wardle <mark@wardle.org>
2024-04-15 21:50:07 +0100

    Remove legacy pc4-ward and htmx experiments

--- src/old/pc4/dev_preload.cljs
+++
@@ -1,6 +0,0 @@
-(ns pc4.dev-preload
-  (:require [taoensso.timbre :as log]))
-
-(js/console.log "Setting logging to :debug. Check browser log levels")
-(log/set-min-level! :debug)
-(log/set-config! taoensso.timbre/default-config)
