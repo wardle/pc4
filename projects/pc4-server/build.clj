d780793d51feb88d9926166dc0f31899c8779038
Mark Wardle <mark@wardle.org>
2024-07-11 18:40:37 +0100

    Fix build

--- projects/pc4-server/build.clj
+++ projects/pc4-server/build.clj
@@ -3,7 +3,7 @@
             [clojure.tools.build.api :as b]))
 
 (def lib 'com.eldrix/pc4)
-(def version (format "1.2.%s" (b/git-count-revs nil)))
+(def version (format "1.1.%s" (b/git-count-revs nil)))
 (def class-dir "target/classes")
 (def basis (delay (b/create-basis {:project "deps.edn"})))
 (def jar-file (format "target/%s-lib-%s.jar" (name lib) version))
@@ -48,36 +48,36 @@
 (defn uber [{:keys [out] :or {out uber-file}}]
   (clean nil)
   (b/compile-clj {:basis        @uber-basis
-                  :src-dirs     ["src/clj"]
-                  :ns-compile   ['com.eldrix.pc4.core]
+                  :ns-compile   ['pc4.server.core]
                   :compile-opts {:elide-meta [:doc :added]}
                   :class-dir    class-dir})
-  (b/copy-file {:src (str "deps.edn")
+  (b/copy-file {:src (str "../../deps.edn")
                 :target (str class-dir "/deps.edn")})
-  (b/copy-file {:src    (str "resources/logback.xml")
+  (b/copy-file {:src    (str "../../components/config/resources/config/config.edn")
+                :target (str class-dir "/config.edn")})
+  (b/copy-file {:src    (str "../../bases/server/resources/logback.xml")
                 :target (str class-dir "/logback.xml")})
-  (b/copy-file {:src    (str "../bases/frontend/resources/public/css/output.css")
+  (b/copy-file {:src    (str "../../components/frontend/resources/public/css/output.css")
                 :target (str class-dir "/public/css/output.css")})
-  (b/copy-file {:src    (str "../bases/frontend/resources/public/js/compiled/manifest.edn")
+  (b/copy-file {:src    (str "../../components/frontend/resources/public/js/compiled/manifest.edn")
                 :target (str class-dir "/public/js/compiled/manifest.edn")})
-  (let [manifest (edn/read-string (slurp "../bases/frontend/resources/public/js/compiled/manifest.edn"))
+  (let [manifest (edn/read-string (slurp "../../components/frontend/resources/public/js/compiled/manifest.edn"))
         output-names (map :output-name manifest)] ;; get a list of all shadow cljs outputs
     (if (pos-int? (count output-names))
       (doseq [output-name output-names]
         (println "Installing cljs frontend module:" output-name)
-        (b/copy-file {:src (str "../bases/frontend/resources/public/js/compiled/" output-name)
+        (b/copy-file {:src (str "../../components/frontend/resources/public/js/compiled/" output-name)
                       :target (str class-dir "/public/js/compiled/" output-name)}))
       (error "no shadow cljs outputs found")))
-  (b/copy-dir {:src-dirs   ["../components/rsdb/resources/rsdb/migrations"]
-               :target-dir (str class-dir "/rsdb/migrations")})
-  (b/copy-dir {:src-dirs   ["src"]
+  (b/copy-dir {:src-dirs   ["../../components/rsdb/resources/migrations"]
+               :target-dir (str class-dir "/migrations")})
+  (b/copy-dir {:src-dirs   ["../../bases/server/src"]
                :target-dir class-dir})
   (b/uber {:class-dir class-dir
            :uber-file (str out)
            :basis     @uber-basis
-           :main      'com.eldrix.pc4.core
+           :main      'pc4.server.core
            :exclude   [#"(?i)^META-INF/license/.*"
                        #"^license/.*"]}))
-
 
 
