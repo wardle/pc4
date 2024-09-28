3eeba90f8854c2aded81a3bb310c5c5222534690
Mark Wardle <mark@wardle.org>
2024-09-28 11:40:40 +0100

    Use new rsdb svc in lemtrada REPL development

--- components/lemtrada/src/pc4/lemtrada/dev.clj
+++ components/lemtrada/src/pc4/lemtrada/dev.clj
@@ -24,6 +24,7 @@
   (def system (ig/init (config/config :pc4-dev) [:pc4.lemtrada.interface/env]))
   (ig/halt! system)
   (def env (:pc4.lemtrada.interface/env system))
+  (def rsdb (:rsdb env))
   (keys env)
   (rsdb/user-by-id (:rsdb env) 1)
   (dmt/write-data env :plymouth)
@@ -40,47 +41,39 @@
 
   (def project-id 126) ;; Cambridge
   (def project-id 127) ;; Plymouth
-  (def conn (:pc4.rsdb.interface/conn system))
   (map (fn [{:t_user/keys [username] :as user}]
-         (let [user (rsdb/create-user! conn user)]
-           (rsdb/register-user-to-project! conn {:username username :project-id project-id})
+         (let [user (rsdb/create-user! rsdb user)]
+           (rsdb/register-user-to-project! rsdb {:username username :project-id project-id})
            user)) users)
-  (def conn (:com.eldrix.rsdb/conn system))
-  (:t_user/id (rsdb/user-by-username conn "xxx"))
-  (:t_user/id (rsdb/user-by-username conn "xxx"))
-  (rsdb/reset-password! conn {:t_user/id 968})
-  (next.jdbc.sql/update! conn :t_user {:credential "xxxx"} {:t_user/id 967})
-  (next.jdbc/execute! (:com.eldrix.rsdb/conn system) ["select * from t_user"])
+  (:t_user/id (rsdb/user-by-username rsdb "xxx"))
+  (:t_user/id (rsdb/user-by-username rsdb "xxx"))
+  (rsdb/reset-password! rsdb {:t_user/id 968})
+  (next.jdbc.sql/update! (:conn rsdb) :t_user {:credential "xxxx"} {:t_user/id 967})
+  (next.jdbc/execute! (:conn rsdb) ["select * from t_user"])
 
-  (rsdb/project-by-id (:com.eldrix.rsdb/conn system) 3)
+  (rsdb/project-by-id rsdb 3)
 
-  (rsdb/register-user-to-project! (:com.eldrix.rsdb/conn system) {:username   "xxx" :project-id 1})
+  (rsdb/register-user-to-project! rsdb {:username   "xxx" :project-id 1})
 
-  (rsdb/set-must-change-password! (:com.eldrix.rsdb/conn system) "xxx")
+  (rsdb/set-must-change-password! rsdb  "xxx")
 
   (rsdb/patient-by-project-pseudonym (:com.eldrix.rsdb/conn system) 3 "xxx")
+  (rsdb/update-legacy-pseudonymous-patient!
+   rsdb 128283
+   {:nhs-number "xxx"
+    :date-birth (java.time.LocalDate/of 1900 1 1)
+    :sex        :FEMALE})
 
-  (def global-salt (get-in system [:pc4.rsdb.interface/config :legacy-global-pseudonym-salt]))
-  global-salt
-  (next.jdbc/with-transaction [txn (:com.eldrix.rsdb/conn system) {:isolation :serializable}]
-    (rsdb/update-legacy-pseudonymous-patient!
-     txn global-salt 128283
-     {:nhs-number "xxx"
-      :date-birth (java.time.LocalDate/of 1900 1 1)
-      :sex        :FEMALE}))
-  (rsdb/find-legacy-pseudonymous-patient (:com.eldrix.rsdb/conn system)
-                                         {:salt       global-salt :project-id 126 :nhs-number "xxx"
-                                          :date-birth (java.time.LocalDate/of 1900 1 1)})
-  (rsdb/project-by-id (:com.eldrix.rsdb/conn system) 126)
+  (rsdb/project-by-id rsdb 126)
 
-  (rsdb/discharge-episode! (:com.eldrix.rsdb/conn system) 1 {:t_episode/id 48256})
+  (rsdb/discharge-episode! rsdb 1 {:t_episode/id 48256})
 
-  (dmt/make-patient-identifiers-table system
-                                      (com.eldrix.pc4.modules.dmt/fetch-study-patient-identifiers system :cambridge))
+  (dmt/make-patient-identifiers-table env
+                                      (dmt/fetch-study-patient-identifiers env :cambridge))
   (dmt/write-table system
-                   com.eldrix.pc4.modules.dmt/patient-identifiers-table
+                   dmt/patient-identifiers-table
                    :plymouth
-                   (com.eldrix.pc4.modules.dmt/fetch-study-patient-identifiers system :plymouth))
+                   (dmt/fetch-study-patient-identifiers system :plymouth))
 
   (dmt/all-patient-diagnoses system [124079])
   (dmt/write-data system :cambridge))
