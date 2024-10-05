fdc0d14580a0aed54e60854b9302cb4414f36736
Mark Wardle <mark@wardle.org>
2024-10-05 16:01:07 +0100

    Add some utility fns for HL7 FHIR data processing
    Ordering, matching and validity with latter based on nested FHIR Period
    data structures.

--- components/fhir/src/pc4/fhir/interface.clj
+++ components/fhir/src/pc4/fhir/interface.clj
@@ -1,16 +1,18 @@
 (ns pc4.fhir.interface
-  "Specifications for pc4 FHIR data structures. Automated conversion from the
-  FHIR specifications would be possible, but the FHIR standard is too open
-  and needs to be constrained in any particular implementation. As such, these
-  specifications tighten the more broad general purpose specifications for use
-  within pc4."
-  (:require [clojure.spec.alpha :as s])
+  "Specifications and utility functions for pc4 FHIR data structures. 
+  Automated conversion from the FHIR specifications would be possible, but
+  the FHIR standard is too open and needs to be constrained in any particular
+  implementation. As such, these specifications tighten the more broad 
+  general purpose specifications for use within pc4."
+  (:require [clojure.spec.alpha :as s]
+            [clojure.core :as c])
   (:import (java.time LocalDate LocalDateTime)))
 
 (def administrative-gender #{"male" "female" "other" "unknown"})
 (def contact-point-system #{"phone" "fax" "email" "pager" "url" "sms" "other"})
 (def contact-point-use #{"home" "work" "temp" "old" "mobile"})
 (def human-name-use #{"anonymous" "maiden" "nickname" "official" "old" "temp" "usual"})
+(def identifier-use #{"usual" "official" "temp" "secondary" "old"})
 
 (s/def :org.hl7.fhir.Attachment/contentType string?)
 (s/def :org.hl7.fhir.Attachment/data string?)
@@ -31,16 +33,20 @@
                 :org.hl7.fhir.Attachment/width
                 :org.hl7.fhir.Attachment/height]))
 
-(s/def :org.hl7.fhir.Identifier/system string?)
-(s/def :org.hl7.fhir.Identifier/value string?)
-(s/def :org.hl7.fhir/Identifier
-  (s/keys :req [:org.hl7.fhir.Identifier/system
-                :org.hl7.fhir.Identifier/value]))
-
 (s/def :org.hl7.fhir.Period/start (s/nilable #(instance? LocalDate %)))
 (s/def :org.hl7.fhir.Period/end (s/nilable #(instance? LocalDate %)))
 (s/def :org.hl7.fhir/Period
   (s/keys :req [:org.hl7.fhir.Period/start :org.hl7.fhir.Period/end]))
+
+(s/def :org.hl7.fhir.Identifier/system string?)
+(s/def :org.hl7.fhir.Identifier/value string?)
+(s/def :org.hl7.fhir.Identifier/use identifier-use)
+(s/def :org.hl7.fhir.Identifier/period :org.hl7.fhir/Period)
+(s/def :org.hl7.fhir/Identifier
+  (s/keys :req [:org.hl7.fhir.Identifier/system
+                :org.hl7.fhir.Identifier/value]
+          :opt [:org.hl7.fhir.Identifier/period
+                :org.hl7.fhir.Identifier/use]))
 
 (s/def :org.hl7.fhir.HumanName/family string?)
 (s/def :org.hl7.fhir.HumanName/given (s/coll-of string?))
@@ -127,3 +133,167 @@
        (filter #(= "https://fhir.hl7.org.uk/Id/gmp-number"
                    (get-in % [:org.hl7.fhir.Reference/identifier :org.hl7.fhir.Identifier/system])))
        (map :org.hl7.fhir.Reference/identifier)))
+
+(defn match-identifier
+  "Returns a predicate to match a FHIR identifier based on information given. 
+  For example,
+  ```
+  (match-identifier :usage \"official\")
+  ```
+  will return a predicate that matches only identifiers with use 'official'."
+  [& {:keys [system value use]}]
+  (fn [{system# :org.hl7.fhir.Identifier/system
+        value#  :org.hl7.fhir.Identifier/value
+        use#    :org.hl7.fhir.Identifier/use}]
+    (and
+     (or (nil? system) (= system system#))
+     (or (nil? value)  (= value value#))
+     (or (nil? use)    (= use use#)))))
+
+(defn match-human-name
+  [& {:keys [use family]}]
+  (fn [{use# :org.hl7.fhir.HumanName/use
+        family# :org.hl7.fhir.HumanName/family}]
+    (and
+     (= use use#)
+     (or (nil? family) (.equalsIgnoreCase family family#)))))
+
+(defn best-match
+  "Determine the 'best' match based on the sequence of predicates specified. 
+  For example, if ids is a sequence of FHIR structures:
+  ```
+  (best-match [(match-identifier :usage \"official\")] ids)
+  ```
+  will return the first official identifier, but if there is none, will
+  fallback to returning the first identifier. Each predicate will be 
+  tried in turn so predicates should be ordered in terms of specificity."
+  ([preds coll]
+   (if-let [pred (first preds)]
+     (if-let [matched (seq (filter pred coll))]
+       (first matched)
+       (recur (rest preds) coll))
+     (first coll))))
+
+(defn by-period
+  "A Comparator of FHIR Period to return in descending order. 
+  A 'nil' start is considered as the distant past, and a 'nil' end is 
+  considered as the distant future."
+  [{s1 :org.hl7.fhir.Period/start
+    e1 :org.hl7.fhir.Period/end}
+   {s2 :org.hl7.fhir.Period/start
+    e2 :org.hl7.fhir.Period/end}]
+  (compare [(or e2 LocalDate/MAX) (or s2 LocalDate/MIN)]
+           [(or e1 LocalDate/MAX) (or s1 LocalDate/MIN)]))
+
+(defn by-periods
+  "Create an indirect comparator that sorts according to
+  period where 'k' is the key for each item to return a 
+  period."
+  [k]
+  (fn [a b] (by-period (k a) (k b))))
+
+(defn valid-period?
+  "Is the FHIR Period valid? A Period is valid on a date if the date
+  is equal or after the start date, and if the date is before the
+  end date. If either start or end are nil, they are assumed to be
+  open limits so that for the purposes of comparison, the start is 
+  in the distant past, and the end in the distant future."
+  ([period]
+   (valid-period? period (LocalDate/now)))
+  ([{:org.hl7.fhir.Period/keys [start end]} ^LocalDate on-date]
+   (let [start# (or start LocalDate/MIN)
+         end# (or end LocalDate/MAX)]
+     (and
+      (or (.isAfter on-date start#) (.isEqual on-date start#))
+      (.isBefore on-date end#)))))
+
+(defn valid-by-period?
+  "Test the validity of structure 'x' by testing the validity of the 
+  FHIR Period returned from 'x' using function (or keyword) 'k'."
+  ([k] (fn [x] (valid-period? (k x))))
+  ([k on-date] (fn [x] (valid-period? (k x) on-date))))
+
+(defn valid-identifiers
+  "Return an ordered sequence of valid FHIR identifiers matching the system specified. 
+  Ordering will be in most recent first."
+  ([system identifiers]
+   (valid-identifiers system (LocalDate/now) identifiers))
+  ([system on-date identifiers]
+   (->> identifiers
+        (filter #(= system (:org.hl7.fhir.Identifier/system %)))                                       ;; limit by system
+        (filter (valid-by-period? :org.hl7.fhir.Identifier/period on-date))
+        (sort (by-periods :org.hl7.fhir.Identifier/period)))))  ;; limit to valid identifiers
+
+(defn best-identifier
+  ([system identifiers]
+   (best-identifier system (LocalDate/now) identifiers))
+  ([system on-date identifiers]
+   (best-match   ;; loop to find 'best' match using these ordered predicates...
+    [(match-identifier :use "official")
+     (match-identifier :use "usual")
+     (match-identifier :use "secondary")]
+    (valid-identifiers system on-date identifiers))))
+
+(defn best-human-name
+  [human-names]
+  (->> human-names
+       (filter (valid-by-period? :org.hl7.fhir.HumanName/period (LocalDate/now)))  ;; limit to valid names
+       (sort (by-periods :org.hl7.fhir.HumanName/period))                                              ;; sort by validity (period) 
+       (best-match                                                ;; now look to find 'best' match using these ordered predicates
+        [(match-human-name :use "official")
+         (match-human-name :use "usual")
+         (match-human-name :use "old")
+         (match-human-name :use "maiden")])))
+
+(comment
+  (def periods
+    [{:id :a
+      :org.hl7.fhir.Period/end (LocalDate/of 2017 1 1)}
+     {:id :b
+      :org.hl7.fhir.Period/start (LocalDate/of 2018 1 1)
+      :org.hl7.fhir.Period/end (LocalDate/of 2020 1 1)}
+     {:id :c
+      :org.hl7.fhir.Period/start (LocalDate/of 2017 1 1)
+      :org.hl7.fhir.Period/end (LocalDate/of 2020 1 1)}
+     {:id :d
+      :org.hl7.fhir.Period/end (LocalDate/of 2018 1 1)}
+     {:id :e
+      :org.hl7.fhir.Period/start (LocalDate/of 2020 1 1)}])
+  (= (sort by-period periods)
+     (sort by-period (shuffle periods))
+     (sort by-period (reverse periods)))
+  (= (mapv :id (sort by-period periods)) [:e :b :c :d :a])
+
+  (def coll
+    [{:org.hl7.fhir.Identifier/system "D"
+      :org.hl7.fhir.Identifier/value "X123456"
+      :org.hl7.fhir.Identifier/period {:org.hl7.fhir.Period/end (LocalDate/of 2022 1 1)}}
+     {:org.hl7.fhir.Identifier/system "D"
+      :org.hl7.fhir.Identifier/value "D123456"}
+     {:org.hl7.fhir.Identifier/system "D"
+      :org.hl7.fhir.Identifier/value "D987654"
+      :org.hl7.fhir.Identifier/period {:org.hl7.fhir.Period/end (LocalDate/of 2024 1 1)}}
+
+     {:org.hl7.fhir.Identifier/system "A"
+      :org.hl7.fhir.Identifier/value "A123456"
+      :org.hl7.fhir.Identifier/period {:org.hl7.fhir.Period/end (LocalDate/of 2022 1 2)}}
+     {:org.hl7.fhir.Identifier/system "B"
+      :org.hl7.fhir.Identifier/value "B123456"
+      :org.hl7.fhir.Identifier/period {:org.hl7.fhir.Period/end (LocalDate/of 2021 1 1)}}
+     {:org.hl7.fhir.Identifier/system "C"
+      :org.hl7.fhir.Identifier/value "C123456"
+      :org.hl7.fhir.Identifier/period {:org.hl7.fhir.Period/end (LocalDate/of 2023 1 1)}}])
+  (sort (by-periods :org.hl7.fhir.Identifier/period) coll)
+  (= (mapv :org.hl7.fhir.Identifier/value (sort (by-periods :org.hl7.fhir.Identifier/period) coll))
+     ["D123456" "D987654" "C123456" "A123456" "X123456" "B123456"])
+  (= (mapv :org.hl7.fhir.Identifier/value (filter (valid-by-period? :org.hl7.fhir.Identifier/period) coll))
+     ["D123456"])
+  (= (mapv :org.hl7.fhir.Identifier/value (sort (by-periods :org.hl7.fhir.Identifier/period)
+                                                (filter (valid-by-period? :org.hl7.fhir.Identifier/period (LocalDate/of 2022 7 1))
+                                                        coll)))
+     ["D123456" "D987654" "C123456"])
+
+  (= "D123456" (:org.hl7.fhir.Identifier/value (best-identifier "D" coll)))
+  (nil? (best-identifier "A" coll))
+  (= "A123456" (:org.hl7.fhir.Identifier/value (best-identifier "A" (LocalDate/of 2020 1 1) coll))))
+
--- components/fhir/test/pc4/fhir/interface_test.clj
+++ components/fhir/test/pc4/fhir/interface_test.clj
@@ -1,6 +1,66 @@
 (ns pc4.fhir.interface-test
-  (:require [clojure.test :as test :refer :all]
-            [pc4.fhir.interface :as fhir]))
+  (:require [clojure.test :as test :refer [deftest is]]
+            [pc4.fhir.interface :as fhir])
+  (:import (java.time LocalDate)))
 
-(deftest dummy-test
-  (is (= 1 1)))
+(def periods
+  [{:id :a
+    :org.hl7.fhir.Period/end (LocalDate/of 2017 1 1)}
+   {:id :b
+    :org.hl7.fhir.Period/start (LocalDate/of 2018 1 1)
+    :org.hl7.fhir.Period/end (LocalDate/of 2020 1 1)}
+   {:id :c
+    :org.hl7.fhir.Period/start (LocalDate/of 2017 1 1)
+    :org.hl7.fhir.Period/end (LocalDate/of 2020 1 1)}
+   {:id :d
+    :org.hl7.fhir.Period/end (LocalDate/of 2018 1 1)}
+   {:id :e
+    :org.hl7.fhir.Period/start (LocalDate/of 2020 1 1)}])
+
+(deftest test-period-sorting
+  (is (= (sort fhir/by-period periods)
+         (sort fhir/by-period (shuffle periods))
+         (sort fhir/by-period (reverse periods))))
+  (is (= (mapv :id (sort fhir/by-period periods)) [:e :b :c :d :a])))
+
+(def coll
+  [{:org.hl7.fhir.Identifier/system "D"
+    :org.hl7.fhir.Identifier/value "X123456"
+    :org.hl7.fhir.Identifier/period {:org.hl7.fhir.Period/end (LocalDate/of 2022 1 1)}}
+   {:org.hl7.fhir.Identifier/system "D"
+    :org.hl7.fhir.Identifier/value "D123456"}
+   {:org.hl7.fhir.Identifier/system "D"
+    :org.hl7.fhir.Identifier/value "D987654"
+    :org.hl7.fhir.Identifier/period {:org.hl7.fhir.Period/end (LocalDate/of 2024 1 1)}}
+
+   {:org.hl7.fhir.Identifier/system "A"
+    :org.hl7.fhir.Identifier/value "A123456"
+    :org.hl7.fhir.Identifier/period {:org.hl7.fhir.Period/end (LocalDate/of 2022 1 2)}}
+   {:org.hl7.fhir.Identifier/system "B"
+    :org.hl7.fhir.Identifier/value "B123456"
+    :org.hl7.fhir.Identifier/period {:org.hl7.fhir.Period/end (LocalDate/of 2021 1 1)}}
+   {:org.hl7.fhir.Identifier/system "C"
+    :org.hl7.fhir.Identifier/value "C123456"
+    :org.hl7.fhir.Identifier/period {:org.hl7.fhir.Period/end (LocalDate/of 2023 1 1)}}])
+
+(deftest test-identifiers
+  (is (= (mapv :org.hl7.fhir.Identifier/value (sort (fhir/by-periods :org.hl7.fhir.Identifier/period) coll))
+         ["D123456" "D987654" "C123456" "A123456" "X123456" "B123456"]))
+  (is (= (mapv :org.hl7.fhir.Identifier/value (filter (fhir/valid-by-period? :org.hl7.fhir.Identifier/period) coll))
+         ["D123456"]))
+  (is (= (->> coll
+              (filter (fhir/valid-by-period? :org.hl7.fhir.Identifier/period (LocalDate/of 2022 7 1)))
+              (sort (fhir/by-periods :org.hl7.fhir.Identifier/period))
+              (mapv :org.hl7.fhir.Identifier/value))
+         ["D123456" "D987654" "C123456"]))
+  (is  (= "D123456" (:org.hl7.fhir.Identifier/value (fhir/best-identifier "D" coll))))
+  (is  (nil? (fhir/best-identifier "A" coll)))
+  (is (= "A123456" (:org.hl7.fhir.Identifier/value (fhir/best-identifier "A" (LocalDate/of 2020 1 1) coll)))))
+
+
+
+
+
+
+
+
