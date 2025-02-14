0443015798d093854bd13bdcebc1b16af3f85879
Mark Wardle <mark@wardle.org>
2025-02-14 21:15:40 +0000

    Whitespace

--- components/graph/src/pc4/graph/interface.clj
+++ components/graph/src/pc4/graph/interface.clj
@@ -1,22 +1,22 @@
 (ns pc4.graph.interface
   (:require
-   [clojure.spec.alpha :as s]
-   [com.wsscode.pathom3.connect.indexes :as pci]
-   [com.wsscode.pathom3.error :as p.error]
-   [com.wsscode.pathom3.interface.eql :as p.eql]
-   [com.wsscode.pathom3.plugin :as p.plugin]
-   [com.wsscode.pathom3.connect.runner :as pcr]
-   [integrant.core :as ig]
-   [pc4.deprivare.interface]
-   [pc4.ods.interface]
-   [pc4.ods-weekly.interface]
-   [pc4.dmd.interface]
-   [pc4.log.interface :as log]
-   [pc4.rsdb.interface]
-   [pc4.graph.users]
-   [pc4.graph.patients]
-   [pc4.msbase.interface]
-   [pc4.snomedct.interface]))
+    [clojure.spec.alpha :as s]
+    [com.wsscode.pathom3.connect.indexes :as pci]
+    [com.wsscode.pathom3.error :as p.error]
+    [com.wsscode.pathom3.interface.eql :as p.eql]
+    [com.wsscode.pathom3.plugin :as p.plugin]
+    [com.wsscode.pathom3.connect.runner :as pcr]
+    [integrant.core :as ig]
+    [pc4.deprivare.interface]
+    [pc4.ods.interface]
+    [pc4.ods-weekly.interface]
+    [pc4.dmd.interface]
+    [pc4.log.interface :as log]
+    [pc4.rsdb.interface]
+    [pc4.graph.users]
+    [pc4.graph.patients]
+    [pc4.msbase.interface]
+    [pc4.snomedct.interface]))
 
 (defmethod ig/init-key ::ops
   [_ ops]
@@ -35,41 +35,41 @@
 
       ;; add plugin to handle resolver errors
       (p.plugin/register
-       {::p.plugin/id `handle-resolver-err
-        ::pcr/wrap-resolver-error
-        (fn [_]
-          (fn [_env node error]
-            (when (instance? Throwable error)
-              (.printStackTrace ^Throwable error))
-            (log/error "pathom resolver error" {:node node :error error})))})
+        {::p.plugin/id `handle-resolver-err
+         ::pcr/wrap-resolver-error
+         (fn [_]
+           (fn [_env node error]
+             (when (instance? Throwable error)
+               (.printStackTrace ^Throwable error))
+             (log/error "pathom resolver error" {:node node :error error})))})
 
       ;; add plugin to handle mutation errors
       (p.plugin/register
-       {::p.plugin/id `handle-mutation-err
-        ::pcr/wrap-mutate
-        (fn [mutate]
-          (fn [env params]
-            (try
-              (mutate env params)
-              (catch Throwable err
-                (.printStackTrace err)
-                {::pcr/mutation-error (ex-message err)}))))})
+        {::p.plugin/id `handle-mutation-err
+         ::pcr/wrap-mutate
+         (fn [mutate]
+           (fn [env params]
+             (try
+               (mutate env params)
+               (catch Throwable err
+                 (.printStackTrace err)
+                 {::pcr/mutation-error (ex-message err)}))))})
 
       ;; add plugin to process query parameters from query for Fulcro clients
       (p.plugin/register
-       {::p.plugin/id `query-params->env
-        ::p.eql/wrap-process-ast
-        (fn [process]
-          (fn [env ast]
-            (let [children     (:children ast)
-                  query-params (reduce
-                                (fn [qps {:keys [type params]}]
-                                  (cond-> qps
-                                    (and (not= :call type) (seq params)) (merge params)))
-                                {}
-                                children)
-                  env          (assoc env :query-params query-params)]
-              (process env ast))))})))
+        {::p.plugin/id `query-params->env
+         ::p.eql/wrap-process-ast
+         (fn [process]
+           (fn [env ast]
+             (let [children (:children ast)
+                   query-params (reduce
+                                  (fn [qps {:keys [type params]}]
+                                    (cond-> qps
+                                      (and (not= :call type) (seq params)) (merge params)))
+                                  {}
+                                  children)
+                   env (assoc env :query-params query-params)]
+               (process env ast))))})))
 
 (s/def ::connect-viz boolean?)
 (s/def ::env map?)
@@ -86,4 +86,3 @@
         (log/info "connecting pathom-viz"))
       (catch Exception _ (log/warn "unable to connect to pathom-viz as dependency not available in this build"))))
   (p.eql/boundary-interface env))
-
