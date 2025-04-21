eb582abd52e0720d22e10b5d25f6f6a6babf15d6
Mark Wardle <mark@wardle.org>
2025-04-21 15:11:39 +0100

    Redundant comments

--- components/chart/src/pc4/chart/edss.clj
+++ components/chart/src/pc4/chart/edss.clj
@@ -53,7 +53,6 @@
                                                        (LocalDate/parse %)
                                                        true
                                                        (catch Exception _ false)))))
-;; Standard EDSS values - defined once for reuse throughout the codebase
 (def valid-edss-values #{0.0 1.0 1.5 2.0 2.5 3.0 3.5 4.0 4.5 5.0 5.5 6.0 6.5 7.0 7.5 8.0 8.5 9.0 9.5 10.0})
 (s/def ::edss (s/nilable (s/and number? valid-edss-values)))
 (s/def ::in-relapse? boolean?)
