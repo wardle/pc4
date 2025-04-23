4202f4ec1b64e04d48f66cdfc98656fe9c65620d
Mark Wardle <mark@wardle.org>
2025-04-23 17:41:50 +0100

    Ensure EDSS chart flexible enough to accept nil for in relapse

--- components/chart/src/pc4/chart/edss.clj
+++ components/chart/src/pc4/chart/edss.clj
@@ -55,7 +55,7 @@
                                                        (catch Exception _ false)))))
 (def valid-edss-values #{0.0 1.0 1.5 2.0 2.5 3.0 3.5 4.0 4.5 5.0 5.5 6.0 6.5 7.0 7.5 8.0 8.5 9.0 9.5 10.0})
 (s/def ::edss (s/nilable (s/and number? valid-edss-values)))
-(s/def ::in-relapse? boolean?)
+(s/def ::in-relapse? (s/nilable boolean?))
 (s/def ::edss-score (s/keys :req-un [::id ::date ::edss]
                             :opt-un [::in-relapse?]))
 (s/def ::edss-scores (s/coll-of ::edss-score))
