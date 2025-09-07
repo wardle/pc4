de74115caeda755cfed627717371921a9a382dc1
Mark Wardle <mark@wardle.org>
2025-09-07 14:46:51 +0100

    Update dependencies and fix test namespace import
    Corrected the namespace import in core_test.clj to use pc4.workbench-server.core. Downgraded flying-saucer-pdf dependency in report component to 9.13.3. Removed the .keep file from projects directory. Cleaned up test alias in workbench-server deps.edn by removing extra-paths.

--- bases/workbench-server/test/pc4/workbench_server/core_test.clj
+++ bases/workbench-server/test/pc4/workbench_server/core_test.clj
@@ -1,6 +1,6 @@
 (ns pc4.workbench-server.core-test
   (:require [clojure.test :as test :refer :all]
-            [pc4.server.core :as core]))
+            [pc4.workbench-server.core :as core]))
 
 (deftest dummy-test
   (is (= 1 1)))
