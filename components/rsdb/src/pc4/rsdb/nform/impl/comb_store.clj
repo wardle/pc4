7d94a8345badef852abd5699261e1e9d42a89d43
Mark Wardle <mark@wardle.org>
2025-09-26 07:35:25 +0100

    Tidy

--- components/rsdb/src/pc4/rsdb/nform/impl/comb_store.clj
+++ components/rsdb/src/pc4/rsdb/nform/impl/comb_store.clj
@@ -1,12 +1,13 @@
 (ns pc4.rsdb.nform.impl.comb-store
   "The combination store is a simple facade delegating to defined concrete store
   implementations."
-  (:require [clojure.spec.alpha :as s]
-            [pc4.rsdb.nform.impl.protocols :as p]
-            [pc4.rsdb.nform.impl.registry :as registry]))
+  (:require
+    [pc4.rsdb.nform.impl.protocols :as p]
+    [pc4.rsdb.nform.impl.registry :as registry]))
 
 (deftype CombinedFormStore
   [nf-store wo-store]
+
   p/FormStore
   (upsert [_ {:keys [form_type] :as form}]
     (if-let [{:keys [store]} (registry/form-definition-by-form-type form_type)]
@@ -14,11 +15,12 @@
         :nf (p/upsert nf-store form)
         :wo (p/upsert wo-store form))
       (throw (ex-info (str "unsupported form; no definition found for form type:" form_type) form))))
+
   (form [_ id]
     (if (uuid? id)
       (p/form nf-store id)
       (p/form wo-store id)))
-  (forms [_ params]
 
+  (forms [_ params]
     (into (or (p/forms nf-store params) [])
           (p/forms wo-store params))))
