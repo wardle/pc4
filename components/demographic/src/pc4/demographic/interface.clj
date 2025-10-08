a78a1714e164446329727b2f6921e2ef757e278a
Mark Wardle <mark@wardle.org>
2025-10-08 19:45:34 +0100

    Add 'patient register' page

--- components/demographic/src/pc4/demographic/interface.clj
+++ components/demographic/src/pc4/demographic/interface.clj
@@ -1,6 +1,7 @@
 (ns pc4.demographic.interface
   "A demographic service is an opaque provider of demographic information. "
   (:require [clojure.spec.alpha :as s]
+            [clojure.string :as str]
             [integrant.core :as ig]
             [pc4.demographic.synthetic :as synth]
             [pc4.demographic.protos :as p]
@@ -13,7 +14,7 @@
 (s/def ::id keyword?)
 (s/def ::title string?)
 (s/def ::svc #(satisfies? p/PatientsByIdentifier %))
-(s/def ::systems (s/coll-of ::system :kind set? :min-count 1))
+(s/def ::systems (s/coll-of ::system :kind vector? :min-count 1))
 (s/def ::remote boolean?)
 
 (s/def ::provider
@@ -21,7 +22,7 @@
           :opt-un [::systems ::remote]))
 
 (s/def ::providers
-  (s/coll-of ::provider :kind vector? :min-count 1))
+  (s/coll-of ::provider))
 
 (s/def ::config
   (s/keys :req-un [::providers]))
@@ -63,7 +64,7 @@
   ([{:keys [providers]} system value {:keys [provider-id only-single-match ask-all-systems]}]
    (log/debug "patients-by-identifier" {:provider-id provider-id :system system :value value})
    (let [by-provider-id (if provider-id (fn [{:keys [id]}] (= id provider-id)) (constantly true))
-         by-system (if ask-all-systems (constantly true) (fn [{:keys [systems]}] ((or systems (constantly true)) system)))]
+         by-system (if ask-all-systems (constantly true) (fn [{:keys [systems]}] (or (not systems) (some #{system} systems))))]
      (loop [providers (filter #(and (by-provider-id %) (by-system %)) providers)]
        (when-let [provider (first providers)]
          (if-let [patients (seq (p/fetch (:svc provider) system value))]
@@ -98,3 +99,74 @@
 (defmethod ig/init-key ::fake-service
   [_ {:keys [conn]}]
   (synth/make-synthetic-provider))
+
+(def fhir-identifier-system-labels
+  "Map of FHIR identifier system URIs to human-readable labels for display in UI."
+  {"https://fhir.nhs.uk/Id/nhs-number"               "NHS Number"
+   "https://fhir.cavuhb.nhs.wales/Id/pas-identifier" "CRN (Cardiff and Vale)"
+   "https://fhir.ctmuhb.nhs.wales/Id/pas-identifier" "CRN (Cwm Taf Morgannwg)"
+   "https://fhir.abuhb.nhs.wales/Id/pas-identifier"  "CRN (Aneurin Bevan)"
+   "https://fhir.sbuhb.nhs.wales/Id/pas-identifier"  "CRN (Swansea Bay)"
+   "https://fhir.hduhb.nhs.wales/Id/pas-identifier"  "CRN (Hywel Dda)"
+   "https://fhir.bcuhb.nhs.wales/Id/pas-identifier"  "CRN (Betsi Cadwaladr)"
+   "https://fhir.powys.nhs.wales/Id/pas-identifier"  "CRN (Powys)"
+   "https://fhir.nhs.wales/Id/empi-number"           "NHS Wales eMPI Number"})
+
+(defn make-system [{:keys [id]} url]
+  {:id    (str (name id) "|" url)
+   :url   url
+   :title (get fhir-identifier-system-labels url)})
+
+(defn available-providers
+  "Return the list of configured external demographic providers.
+
+  Parameters:
+  - svc: Demographic service from Integrant configuration
+
+  Returns sequence of provider maps, each containing:
+  - :id      - provider identifier keyword (e.g., :wales-cav-pms)
+  - :title   - human-readable provider name
+  - :svc     - the provider service implementation
+  - :systems - a sequence of systems (:id :title :url); can be nil
+  - :remote  - boolean indicating if this is a remote service
+
+  Example:
+  ```
+  (available-providers svc)
+  =>
+  [{:id :wales-cav-pms
+    :title \"Synthetic CAVUHB PMS\"
+    :svc #<..>
+    :systems [{:url \"https://fhir.nhs.uk/Id/nhs-number\", :title \"NHS Number\"}
+              {:url \"https://fhir.cavuhb.nhs.wales/Id/pas-identifier\", :title \"CRN (Cardiff and Vale)\"}]}
+    {:id :wales-empi
+     :title \"Synthetic NHS Wales eMPI\"
+     :svc #<..>
+     :systems nil
+     :remote true}]
+  ```"
+  [{:keys [providers]}]
+  (mapv
+    (fn [provider]
+      (update provider :systems
+              (fn [urls]
+                (when (seq urls)
+                  (mapv (fn [url]
+                          (make-system provider url))
+                        urls)))))
+    providers))
+
+(defn parse-provider-system-id
+  "Parse composite provider-system id back into provider-id and system.
+
+  Format: 'provider-id|system-url' or just 'provider-id' if no system."
+  [s]
+  (let [[provider-id system] (str/split s #"\|" 2)]
+    (cond-> {:provider-id (keyword provider-id)}
+      system (assoc :system system))))
+
+(comment
+  (parse-provider-system-id "floo")
+  (require '[pc4.config.interface :as config])
+  (config/config :dev)
+  (available-providers (::svc (config/config :dev))))
